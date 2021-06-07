986792b096227900c340c292b1e742fea63209c1
Ryan Guest <ryan@extramilecommunications.com>
2021-06-07 11:35:42 +0100

    Livereload added

--- tickets/static/gulpfile.js
+++ tickets/static/gulpfile.js
@@ -4,6 +4,7 @@ var gulpless = require('gulp-less');
 var gulpautoprefixer = require('gulp-autoprefixer');
 var gulpplumber = require('gulp-plumber');
 var babel = require('gulp-babel')
+var livereload = require('gulp-livereload')
 
 //Creating a Style task that convert LESS to CSS
 gulp.task('styles',function(){
@@ -14,7 +15,8 @@ gulp.task('styles',function(){
      .pipe(gulpplumber())
      .pipe(gulpless())
      .pipe(gulpautoprefixer())
-     .pipe(gulp.dest(output));
+     .pipe(gulp.dest(output))
+     .pipe(livereload())
 });
 
 //Creating a JavaScript task that compiles JS to ES5 (for minification)
@@ -26,10 +28,12 @@ gulp.task('javascript2es5', function(){
       .pipe(gulpplumber())
       .pipe(babel())
       .pipe(gulp.dest(out))
+      .pipe(livereload())
 })
 
 //Watch task to update and compile when changes are made.
 gulp.task('watch', function(){
+  livereload.listen();
   gulp.watch('less/*.less', ['styles']);
   gulp.watch('less/components/*.less', ['styles']);
   gulp.watch('js/*.js', ['javascript2es5'])
