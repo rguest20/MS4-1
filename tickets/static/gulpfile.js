e7c3e57faf21e19b15f41b9ee6c5fb8a43a8add4
Ryan Guest <ryan@extramilecommunications.com>
2021-06-07 11:19:22 +0100

    Setup gulp to watch for js changes.

--- tickets/static/gulpfile.js
+++ tickets/static/gulpfile.js
@@ -1,32 +1,36 @@
+//Dependencies
 var gulp = require('gulp');
 var gulpless = require('gulp-less');
 var gulpautoprefixer = require('gulp-autoprefixer');
 var gulpplumber = require('gulp-plumber');
 var babel = require('gulp-babel')
+
 //Creating a Style task that convert LESS to CSS
 gulp.task('styles',function(){
-    var srcfile = './less/style.less';
-    var output = './css';
-         return gulp
-                 .src(srcfile)
-                 .pipe(gulpplumber())
-                 .pipe(gulpless())
-                 .pipe(gulpautoprefixer())
-                 .pipe(gulp.dest(output));
-  });
+  var srcfile = './less/style.less';
+  var output = './css';
+   return gulp
+     .src(srcfile)
+     .pipe(gulpplumber())
+     .pipe(gulpless())
+     .pipe(gulpautoprefixer())
+     .pipe(gulp.dest(output));
+});
 
-gulp.task('javascript', function(){
+//Creating a JavaScript task that compiles JS to ES5 (for minification)
+gulp.task('javascript2es5', function(){
   var src = './js/custom.js';
   var out = './js/dist/';
     return gulp
-              .src(src)
-              .pipe(gulpplumber())
-              .pipe(babel())
-              .pipe(gulp.dest(out))
+      .src(src)
+      .pipe(gulpplumber())
+      .pipe(babel())
+      .pipe(gulp.dest(out))
 })
 
+//Watch task to update and compile when changes are made.
 gulp.task('watch', function(){
   gulp.watch('less/*.less', ['styles']);
   gulp.watch('less/components/*.less', ['styles']);
-  gulp.watch('js/*.js', ['javascript'])
+  gulp.watch('js/*.js', ['javascript2es5'])
 })
