20f085dd1679fa2bfb9b24af54d11d24ee4f08b7
Ryan Guest <ryan@extramilecommunications.com>
2021-07-25 20:21:16 +0100

    Static files routed

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, reverse_lazy, include
 from django.contrib.auth import views as auth_views
-
+from django.views.static import serve
 from . import views
 
 app_name = 'ticketing'
@@ -37,4 +37,5 @@ urlpatterns = [
     path('cancelled/', views.cancelled, name='cancelled'),
     path('success/', views.success, name='success'),
     path('deleted/', views.deleted, name='deleted'),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
