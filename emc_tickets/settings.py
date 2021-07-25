bf0eda44ab83bb4f02faa3251d467ec4e20ef6c7
Ryan Guest <ryan@extramilecommunications.com>
2021-07-25 15:35:30 +0100

    Secret key changed and added to env variables

--- emc_tickets/settings.py
+++ emc_tickets/settings.py
@@ -26,7 +26,7 @@ MEDIA_URL = '/media/'
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=wi^srfjq-*wy506x0+qvx&q6a-k79dybq3f1m3_$!k1j*y7(0'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
