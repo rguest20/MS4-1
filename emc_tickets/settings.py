056f9eccfc6e595248ee88d264f4697a4616329b
Ryan Guest <ryan@extramilecommunications.com>
2021-07-25 20:23:15 +0100

    add repath

--- emc_tickets/settings.py
+++ emc_tickets/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
-from pathlib import Path
+from pathlib import Path, re_path
 from dotenv import load_dotenv
 load_dotenv()
 
