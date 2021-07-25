6adcd998da97e81b8a0fda816a456febfa7fcf62
Ryan Guest <ryan@extramilecommunications.com>
2021-07-25 16:04:33 +0100

    Setup Postgres

--- emc_tickets/settings.py
+++ emc_tickets/settings.py
@@ -89,10 +89,23 @@ WSGI_APPLICATION = 'emc_tickets.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': os.environ.get('POSTGRES_DB_NAME'),
+
+        'USER': os.environ.get('POSTGRES_DB_USERNAME'),
+
+        'PASSWORD': os.environ.get('POSTGRES_DB_PASSWORD'),
+
+        'HOST': os.environ.get('POSTGRES_DB_HOST'),
+
+        'PORT': os.environ.get('POSTGRES_DB_PORT'),
+
     }
+
 }
 
 
