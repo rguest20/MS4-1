7a3147edbc4a3088dbdc39e89fc12c49b5d58ff0
Ryan Guest <ryan@extramilecommunications.com>
2021-07-25 16:14:12 +0100

    Updated allowed hosts

--- emc_tickets/settings.py
+++ emc_tickets/settings.py
@@ -33,7 +33,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+'ms4-rguest.herokuapp.com'
+]
 
 
 # Application definition
