628bd1765685d200b766c71bf0c9b90f2fff05b7
Ryan Guest <ryan@extramilecommunications.com>
2021-07-30 16:03:05 +0100

    Debug On

--- emc_tickets/settings.py
+++ emc_tickets/settings.py
@@ -33,7 +33,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
 'localhost',
