12368f6b1bc0b51e5a6b788eb8dc86eff12ed6a6
Ryan Guest <ryan@extramilecommunications.com>
2021-09-09 15:32:24 +0100

    autopep8 on purchasing app

--- purchasing/views.py
+++ purchasing/views.py
@@ -22,12 +22,14 @@ def buy(request):
 
     company = Client.objects.filter(user=request.user).first()
 
-    return render(request,'tickets/buy.html', {'company':company})
+    return render(request, 'tickets/buy.html', {'company': company})
+
 
 def stripe_config(request):
     if request.method == 'GET':
         stripe_config = {'publicKey': settings.STRIPE_PUBLISHABLE_KEY}
         return JsonResponse(stripe_config, safe=False)
+
 
 @csrf_exempt
 def create_checkout_session(request):
@@ -44,26 +46,36 @@ def create_checkout_session(request):
             # [customer] - if you have an existing Stripe Customer ID
             # [payment_intent_data] - capture the payment later
             # [customer_email] - prefill the email input in the form
-            # For full details see https://stripe.com/docs/api/checkout/sessions/create
+            # For full details see
+            # https://stripe.com/docs/api/checkout/sessions/create
 
             # ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
             checkout_session = stripe.checkout.Session.create(
-                success_url=domain_url + 'success?session_id={CHECKOUT_SESSION_ID}',
-                cancel_url=domain_url + 'cancelled/',
+                success_url=domain_url +
+                'success?session_id={CHECKOUT_SESSION_ID}',
+                cancel_url=domain_url +
+                'cancelled/',
                 payment_method_types=['card'],
                 mode='payment',
-                client_reference_id= request.user.username,
-                line_items=[{'quantity': dict, 'price_data': {'currency': 'GBP', 'product': 'prod_JutS7RlYuaxtDj', 'unit_amount': productprice.unit_amount}}]
-            )
+                client_reference_id=request.user.username,
+                line_items=[
+                    {
+                        'quantity': dict,
+                        'price_data': {
+                            'currency': 'GBP',
+                            'product': 'prod_JutS7RlYuaxtDj',
+                            'unit_amount': productprice.unit_amount}}])
             return JsonResponse({'sessionId': checkout_session['id']})
         except Exception as e:
             return JsonResponse({'error': str(e)})
 
+
 def cancelled(request):
-    return render(request,'tickets/stripe/cancelled.html', {})
+    return render(request, 'tickets/stripe/cancelled.html', {})
+
 
 def success(request):
-    return render(request,'tickets/stripe/success.html', {})
+    return render(request, 'tickets/stripe/success.html', {})
 
 
 @csrf_exempt
@@ -74,10 +86,10 @@ def stripe_webhook(request):
 
     try:
         event = stripe.Event.construct_from(
-        json.loads(payload), stripe.api_key
+            json.loads(payload), stripe.api_key
         )
     except ValueError as e:
-    # Invalid payload
+        # Invalid payload
         return HttpResponse(status=400)
 
     # Handle the event
@@ -85,7 +97,8 @@ def stripe_webhook(request):
         session = event.data.object
         hoursbought = session.amount_total / 10000
         customer_email = session.customer_details.email
-        customer = Client.objects.filter(name = session.client_reference_id).first()
+        customer = Client.objects.filter(
+            name=session.client_reference_id).first()
         customer.paid_extra_hours += hoursbought
         customer.save()
     else:
